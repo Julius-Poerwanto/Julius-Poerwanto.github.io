2b5ebc074c825cf248ff1b16df83a2c008a30b0b
Julius Poerwanto <julius.poerwanto@binus.ac.id>
2023-05-25 23:46:19 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'dark': '#222831',
+      'main': '#00ADB5',
+      'light': '#EEEEEE',
+      'dark2': '#1E1E1E',
+      'green': '#00CF5F',
+      'gray': '#393E46'
+    },
+    extend: {},
+  },
+  plugins: [],
+}
